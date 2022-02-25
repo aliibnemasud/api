60fde761313b3f7d7532efab98fb630fe286a926
aliibnemasud <mdmasudrony@gmail.com>
2022-02-25 16:04:56 +0600

    Buddies api adeed

---
+++ js/buddeis.js
@@ -0,0 +1,21 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=50')
+    .then(res => res.json())
+    .then(data => buddies(data));
+}
+
+loadBuddies();
+
+const buddies = data => {    
+    const buddiesc = data.results;
+    const rBuddies = document.getElementById('buddies');
+    for (const buddy of buddiesc){        
+        const p = document.createElement('p');
+        p.innerText = buddy.email;
+        rBuddies.appendChild(p);    
+
+    }
+}
+
+
+
